620c2f77bfce2b7a472d2c8a25799e13f000f188
Abigail <4estgirl322@gmail.com>
2022-02-04 21:27:54 -0500

    Updated most buttons event listeners to update displayArray in addition to display

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let solution = "test";
 let display = document.querySelector("#displayValue");
+let displayArray = [];
 
 //basic math functions
 function add (num1, num2){
@@ -36,25 +37,34 @@ function operate (num1, operator, num2){
     return solution;
 };
 
-//event listeners that update the display when buttons are clicked
+
+//event listeners that update both display and display value array
 let numbers = document.querySelectorAll(".numberButton");
 numbers.forEach(button => {
     button.addEventListener("click", () => {
         display.textContent += button.textContent;
+        displayArray.push(button.textContent);
+        console.log(displayArray);
     });
 });
 let clear = document.querySelector("#clear");
 clear.addEventListener("click", () => {
     display.textContent = "";
+    displayArray = [];
+    console.log(displayArray);
 });
 let backspace = document.querySelector("#back");
 backspace.addEventListener("click", () => {
     display.textContent = display.textContent.slice(0,-1);
+    displayArray.pop();
+    console.log(displayArray);
 });
 let operators = document.querySelectorAll(".operatorButton");
 operators.forEach(button => {
     button.addEventListener("click", () => {
         display.textContent += button.textContent;
+        displayArray.push(button.textContent);
+        console.log(displayArray);
     });
 });
 let parens = document.querySelector("#parens");
@@ -63,10 +73,16 @@ parens.addEventListener("click", () =>{
     let close = display.textContent.lastIndexOf(")");
     if (((open == -1) && (close = -1)) || (open < close)){
         display.textContent += "(";
+        displayArray.push("(");
+        console.log(displayArray);
     } else {
         display.textContent += ")";
+        displayArray.push(")");
+        console.log(displayArray);
     };
 });
+
+//event listeners that update the display when buttons are clicked
 let percent = document.querySelector("#percent");
 percent.addEventListener("click", () => {
     if(display.textContent.length == 0){
